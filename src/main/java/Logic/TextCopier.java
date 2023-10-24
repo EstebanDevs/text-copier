0652488dd9fc1247ac1b9ca44dd9a5bb8cd92595
Esteban Perafan <bestebanperafan@gmail.com>
2023-10-24 20:40:56 -0300

    Finish text copier project

---
+++ src/main/java/Logic/TextCopier.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Logic;
+
+import GUI.Screen;
+
+/**
+ *
+ * @author Esteban
+ */
+public class TextCopier {
+
+    public static void main(String[] args) {
+        Screen GUI = new Screen();
+        GUI.setVisible(true);
+        
+        //Colocar la GUI en el medio de nuestra pantalla
+        GUI.setLocationRelativeTo(null);
+    }
+}
